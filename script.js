f7b082a745a75fbb195e6c553a564c690db5a1d1
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-01 16:33:20 -0400

    nice

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 import { mounted, render, mut, sig, mem, eff_on, each, if_then } from "./chowk/monke.js"
 import { hdom } from "./chowk/hdom/index.js"
+import { drag } from "./drag.js"
 
-let range = (num) => Array(num).fill(0)
 let data = await fetch("./data.json").then(res => res.json())
 
+
+let selectedvideo = sig(undefined)
+
 function Root() {
+	function playvideo(src) {
+		selectedvideo("./" + src)
+		setTimeout(() => {
+			play()
+		}, 100)
+	}
+
+	let ref = (e) => ref = e
+	let videoref = (e) => videoref = e
+
+	let play = () => videoref.play()
+
+	let top = sig(0)
+	let left = sig(0)
+
+	eff_on(selectedvideo, () => {
+		if (selectedvideo()) {
+			top(5)
+		}
+		else { top(-5000) }
+	})
+
+	mounted(() => { drag(ref, { set_left: left, set_top: top }) })
+
 	return hdom([".container",
 		[".videos",
-			data.videofiles.map((e) => ["video", { src: "./" + e }])
+			data.videofiles.reverse()
+				.map((src) => ["p.video",
+					{ onclick: () => playvideo(src) },
+					src.replace("clips/", "").replace("clips2/", "")])
 		],
 
 		[".audios",
-			data.audiofiles.map((e) => ["p", e])
-		]
+			data.audiofiles.map((e) => ["p.audio", e.replace("audio/", "")])
+		],
+
+		[".window", { style: mem(() => `position: fixed; top: ${top()}px; left: ${left()}px;`), ref },
+			["video", { ref: videoref, src: selectedvideo }]]
 	])
 }
 
