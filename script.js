76654f238def0696572b0a0626abbf5dfdbf7b0e
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-01 15:59:28 -0400

    updeets

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 import { mounted, render, mut, sig, mem, eff_on, each, if_then } from "./chowk/monke.js"
 import { hdom } from "./chowk/hdom/index.js"
 
+let range = (num) => Array(num).fill(0)
+let data = await fetch("./data.json").then(res => res.json())
+
 function Root() {
-	return hdom(["h1", "hello world"])
+	return hdom([".container",
+		[".videos",
+			data.videofiles.map((e) => ["video", { src: "./" + e }])
+		],
+
+		[".audios",
+			data.audiofiles.map((e) => ["p", e])
+		]
+	])
 }
 
 render(Root, document.body)
