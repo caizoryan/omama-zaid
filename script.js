cafec54f0fd34533ef79dda268c347dd18ea8667
Aaryan Pashine <pashineaaryan@gmail.com>
2025-05-01 17:21:59 -0400

    ok ok ok

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ let data = await fetch("./data.json").then(res => res.json())
 
 
 let selectedvideo = sig(undefined)
+let selectedaudio = sig(undefined)
+let audios = []
 
 function Root() {
 	function playvideo(src) {
 		selectedvideo("./" + src)
+		let index = Math.floor(Math.random() * data.audiofiles.length)
+		selectedaudio("./" + data.audiofiles[index])
+
+		let audio = new Audio()
+		audio.src = selectedaudio()
+		audios.push(audio)
+
 		setTimeout(() => {
 			play()
+			audio.play()
 		}, 100)
 	}
 
@@ -25,9 +35,21 @@ function Root() {
 
 	eff_on(selectedvideo, () => {
 		if (selectedvideo()) {
-			top(5)
+			top(window.innerHeight * .05)
+			left(window.innerWidth * .05)
 		}
 		else { top(-5000) }
+	})
+
+	eff_on(selectedaudio, () => {
+		let selects = document.querySelectorAll('p.audio')
+
+		selects.forEach((e) => {
+			if (e.getAttribute("selection") == "true") {
+				e.scrollIntoView({ behavior: "smooth" })
+			}
+		})
+
 	})
 
 	mounted(() => { drag(ref, { set_left: left, set_top: top }) })
@@ -36,17 +58,22 @@ function Root() {
 		[".videos",
 			data.videofiles.reverse()
 				.map((src) => ["p.video",
-					{ onclick: () => playvideo(src) },
+					{
+						onclick: () => playvideo(src),
+						selection: mem(() => selectedvideo() == "./" + src ? "true" : "false"),
+					},
 					src.replace("clips/", "").replace("clips2/", "")])
 		],
 
 		[".audios",
-			data.audiofiles.map((e) => ["p.audio", e.replace("audio/", "")])
+			data.audiofiles.map((src) => ["p.audio", {
+				selection: mem(() => selectedaudio() == "./" + src ? "true" : "false"),
+			}, src.replace("audio/", "")])
 		],
 
 		[".window", { style: mem(() => `position: fixed; top: ${top()}px; left: ${left()}px;`), ref },
 			["button.tl", { onclick: () => selectedvideo(null) }, "close"],
-			["video", { ref: videoref, src: selectedvideo }]]
+			["video", { loop: true, muted: true, ref: videoref, src: selectedvideo }]]
 	])
 }
 
